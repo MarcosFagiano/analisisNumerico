13ad2208947ff753934ed3c3daef5e8f07ba0641
MarcosFagiano <2112752@ucc.edu.ar>
2023-10-24 23:29:09 -0300

    runge_kutta

--- ecDif/runge_kutta.cpp
+++ ecDif/runge_kutta.cpp
@@ -18,6 +18,32 @@ double rungeKutta2ndOrder(double x0, double y0, double h, double x_target) {
     return y0;
 }
 
+double rungeKutta3rdOrder(double x0, double y0, double h, double x_target) {
+    while (x0 < x_target) {
+        double k1 = h * f(x0, y0);
+        double k2 = h * f(x0 + 0.5 * h, y0 + 0.5 * k1);
+        double k3 = h * f(x0 + h, y0 - k1 + 2 * k2);
+
+        y0 = y0 + (1.0 / 6.0) * (k1 + 4 * k2 + k3);
+        x0 = x0 + h;
+    }
+    return y0;
+}
+
+double rungeKutta4thOrder(double x0, double y0, double h, double x_target) {
+    while (x0 < x_target) {
+        double k1 = h * f(x0, y0);
+        double k2 = h * f(x0 + 0.5 * h, y0 + 0.5 * k1);
+        double k3 = h * f(x0 + 0.5 * h, y0 + 0.5 * k2);
+        double k4 = h * f(x0 + h, y0 + k3);
+
+        y0 = y0 + (1.0 / 6.0) * (k1 + 2 * k2 + 2 * k3 + k4);
+        x0 = x0 + h;
+    }
+    return y0;
+}
+
+
 int main() {
     double xa = 0.0; // Valor inicial de x
     double y0 = 2.0; // Valor inicial de y
