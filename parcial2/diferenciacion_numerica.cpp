5acbb2fe5d910626035818c43b47a21917413f4d
MarcosFagiano <2112752@ucc.edu.ar>
2023-10-24 22:50:17 -0300

    parcial 2

--- parcial2/diferenciacion_numerica.cpp
+++ parcial2/diferenciacion_numerica.cpp
@@ -3,7 +3,7 @@
 
 // Función f(x) = e^x * cos(x)
 double f(double x) {
-    return std::exp(x) * std::cos(x);
+    return (x*x*x+3.0*x-2.0)/(x*x-4.0);
 }
 
 // Función para calcular la derivada numérica usando la fórmula de cinco puntos
@@ -15,11 +15,11 @@ int main() {
     double h = 0.10000; // El paso es 0.1
 
     // Calcular la derivada en los extremos del intervalo
-    double x1 = 0.0;
-    double x2 = 0.7;
+    double x1 = -1.0;
+    double x2 = 1.0;
 
     for(double x=x1; x<=x2; x+=h){
-      std::cout << "x; " << x << " fx: " << f(x)<< " x': " << DerivadaCincoPuntos(x, h) << std::endl;
+      std::cout << "x; " << x << " x': " << DerivadaCincoPuntos(x, h) << std::endl;
     }
 
     return 0;
